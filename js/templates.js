aa8e4b9d6b05706630afc93fb9011f6582a47c59
Wojciech Grabowski <wojciech@dell.(none)>
2014-02-18 22:49:14 +0100

    improved GUI, added icon fonts (Entypo)

--- js/templates.js
+++ js/templates.js
@@ -18,7 +18,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.Priority) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.Priority); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\" draggable=\"true\">\n	<div class=\"summary\">\n		";
+    + "\" draggable=\"true\">\n	<div class=\"summary\" contenteditable=\"true\">\n		";
   if (helper = helpers.Summary) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.Summary); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
